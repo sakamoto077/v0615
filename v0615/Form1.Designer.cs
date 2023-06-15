d965d6b70b96fc73dba4d59bbba7cd6eb56490aa
sakamoto <130636519+sakamoto077@users.noreply.github.com>
2023-06-15 16:09:38 +0900

    演習14-1

--- v0615/Form1.Designer.cs
+++ v0615/Form1.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(66, 100);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(114, 155);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(234, 100);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 2;
+            label3.Text = "label3";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(273, 196);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(462, 334);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private Button button1;
     }
 }
--- v0615/Form1.cs
+++ v0615/Form1.cs
@@ -1,10 +1,94 @@
+using System.Runtime.Intrinsics.X86;
+
 namespace v0615
 {
     public partial class Form1 : Form
     {
+        static Random rand = new Random();
+        int[] vx = new int[3];
+        int[] vy = new int[3];
+
+
         public Form1()
         {
             InitializeComponent();
+
+            vx[0] = rand.Next(-10, 11);
+            vy[0] = rand.Next(-10, 11);
+            vx[1] = rand.Next(-10, 11);
+            vy[1] = rand.Next(-10, 11);
+            vx[2] = rand.Next(-10, 11);
+            vy[2] = rand.Next(-10, 11);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx[0];
+            label1.Top += vy[0];
+            if (label1.Left < 0)
+            {
+                vx[0] = Math.Abs(vx[0]);
+            }
+            else if (label1.Right > ClientSize.Width)
+            {
+                vx[0] = -Math.Abs(vx[0]);
+            }
+            if (label1.Top < 0)
+            {
+                vy[0] = Math.Abs(vy[0]);
+            }
+            else if (label1.Bottom > ClientSize.Height)
+            {
+                vy[0] = -Math.Abs(vy[0]);
+            }
+
+            label2.Left += vx[1];
+            label2.Top += vy[1];
+            if (label2.Left < 0)
+            {
+                vx[1] = Math.Abs(vx[1]);
+            }
+            else if (label2.Right > ClientSize.Width)
+            {
+                vx[1] = -Math.Abs(vx[1]);
+            }
+            if (label2.Top < 0)
+            {
+                vy[1] = Math.Abs(vy[1]);
+            }
+            else if (label2.Bottom > ClientSize.Height)
+            {
+                vy[1] = -Math.Abs(vy[1]);
+            }
+            label3.Left += vx[2];
+            label3.Top += vy[2];
+            if (label3.Left < 0)
+            {
+                vx[2] = Math.Abs(vx[2]);
+            }
+            else if (label3.Right > ClientSize.Width)
+            {
+                vx[2] = -Math.Abs(vx[2]);
+            }
+            if (label3.Top < 0)
+            {
+                vy[2] = Math.Abs(vy[2]);
+            }
+            else if (label3.Bottom > ClientSize.Height)
+            {
+                vy[2] = -Math.Abs(vy[2]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            for (; a < 10; a++) 
+            {
+                MessageBox.Show($"{a}");
+            }
+
+            MessageBox.Show($"done {a}");
         }
     }
 }
